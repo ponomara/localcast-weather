deeb3f098db673891a3df869aa1ac29f2a53863d
aponoma <siteandstock@gmail.com>
2022-11-06 23:13:11 +0100

    Karma tests complete

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,3 +1,4 @@
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CurrentWeatherComponent } from './current-weather.component';
@@ -8,7 +9,8 @@ describe('CurrentWeatherComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CurrentWeatherComponent ]
+      declarations: [ CurrentWeatherComponent ],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
 
